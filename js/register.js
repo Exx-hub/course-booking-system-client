b5a994998b3d1675c8ea6ecee8af64072f9774ab
Glitch (hello-webpage) <none>
2021-04-21 10:46:50 +0000

    👦💶 Checkpoint ./js/register.js:29751439/198

--- js/register.js
+++ js/register.js
@@ -3,4 +3,9 @@ const registerForm = document.querySelector('#register-form');
 registerForm.addEventListener('submit', (e) => {
   e.preventDefault();
   
+  console.log(e)
+  // registration process
+  // get all the values of the fields needed to create a new user (as in your schema)
+  
+  
 })
