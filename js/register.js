3d17dccfee2c262b16b0fb396a4f7bb5b3c71391
Glitch (hello-webpage) <none>
2021-04-21 10:59:01 +0000

    😄🚴 Checkpoint ./js/register.js:29751439/780

--- js/register.js
+++ js/register.js
@@ -3,9 +3,19 @@ const registerForm = document.querySelector('#register-form');
 registerForm.addEventListener('submit', (e) => {
   e.preventDefault();
   
-  console.log(e)
+
   // registration process
   // get all the values of the fields needed to create a new user (as in your schema)
   
+const firstName = document.querySelector('#first-name').value;
+const lastName = document.querySelector('#last-name').value;
+const mobileNumber = document.querySelector('#mobile-number').value;
+const emailAddress = document.querySelector('#email-address').value;
+const password = document.querySelector('#password').value;
+const confirmPassword = document.querySelector('#confirm-password').value;
+  
+
+// simple data validation 
+const isUserDataValid = (firstName !== "" && lastName !== "" && mobileNumber.length === 11 && emailAddress !== "" && password === confirmPassword && password !== "" && confirmPassword !== "") 
   
 })
