609e663212018b0b9b5d98708244ed150eb8495e
Glitch (hello-webpage) <none>
2021-04-26 10:48:56 +0000

    🤗🏪 Checkpoint ./js/courses.js:29751439/101 ./courses.html:29751439/9376

--- js/courses.js
+++ js/courses.js
@@ -1,4 +1,11 @@
 let isAdmin = localStorage.getItem("isAdmin");
 console.log(isAdmin)
 
-let userId = localStorage.getItem("")
+let userId = localStorage.getItem("userId");
+console.log(userId)
+
+if(!isAdmin || isAdmin === "false") {
+    
+} else {
+  
+}
