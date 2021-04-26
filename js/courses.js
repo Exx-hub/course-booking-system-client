fdad42cf086784b5dffa411d4c5b3b202d9b910d
Glitch (hello-webpage) <none>
2021-04-26 12:45:57 +0000

    ✍️👑 Checkpoint ./js/script.js:29751439/343 ./js/courses.js:29751439/44

--- js/courses.js
+++ js/courses.js
@@ -1,8 +1,5 @@
 const createCourse = document.querySelector("#admin-button");
 
-console.log(isAdmin);
-console.log(userId);
-
 if (!isAdmin || isAdmin === "false") {
   createCourse.innerHTML = null;
 }
--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ let isAdmin = localStorage.getItem("isAdmin");
 // console.log(isAdmin)
 
 let userId = localStorage.getItem("userId");
-console.log(typeof userId)
-console.log(userId)
+
 
 
 // select all nav elements if user is logged in
@@ -15,6 +14,9 @@ let userOutNavs = document.querySelectorAll('.userOut');
 console.log(userOutNavs)
 
 
-if(userId !== null){
+if(userId !== null && typeof userId === 'string'){
   
+  userInNavs.forEach(item => item.classList.toggle("hidden"));
+ 
+  userOutNavs.forEach(item => item.classList.toggle("hidden"));
 }
