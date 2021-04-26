2d7fa801c6b144f9b0d34559994c7a815dd57df8
Glitch (hello-webpage) <none>
2021-04-26 12:34:49 +0000

    ✍️🌋 Checkpoint ./index.html:29751439/94 ./js/script.js:29751439/513 ./css/style.css:29751439/127

--- js/script.js
+++ js/script.js
@@ -6,6 +6,15 @@ console.log(typeof userId)
 console.log(userId)
 
 
+// select all nav elements if user is logged in
+let userInNavs = document.querySelectorAll('.userIn');
+console.log(userInNavs)
+
+// select all nav elements if user is logged out
+let userOutNavs = document.querySelectorAll('.userOut');
+console.log(userOutNavs)
+
+
 if(userId !== null){
   
 }
