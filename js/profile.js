61bbc7f972fcd064eb08fdbb29d083fcc2eb3e07
Glitch (hello-webpage) <none>
2021-04-27 12:36:30 +0000

    🍀✌️ Checkpoint ./js/profile.js:29751439/9368

--- js/profile.js
+++ js/profile.js
@@ -23,8 +23,33 @@ if (uId === null && typeof uId !== "string") {
         mobileNumber,
         enrollments
       } = data.userDetails;
-    
-    // Display data on fronend
-      profileContainer.innerHTML = 
+
+      // Display data on fronend
+      profileContainer.innerHTML = `
+              <div class="col-md-12">
+              <section class="jumbotron my-5">
+              <div class="mb-3">
+              <label for="exampleFormControlInput1" class="form-label">Email address</label>
+              <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+              </div>
+              <h3 class="text-center">First Name: ${firstName}</h3>
+              <h3 class="text-center">Last Name: ${lastName}</h3>
+              <h3 class="text-center">Email: ${emailAddress}</h3>
+              <h3 class="mt-5">Enrollment History</h3>
+                <table class="table">
+                <thead>
+                <tr>
+                  <th> Course ID </th>
+                  <th> Enrolled On </th>
+                  <th> Status </th>
+                </tr>
+                </thead>
+                  <tbody>
+                    ${enrollments}
+                  </tbody>
+                </table> 
+            </section>
+            </div>
+      `;
     });
 }
