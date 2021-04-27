87655ece9ce49f7805299b5ba7572f54f308e7f6
Glitch (hello-webpage) <none>
2021-04-27 13:32:47 +0000

    🏠💪 Checkpoint ./js/new_course.js:29751439/552

--- js/new_course.js
+++ js/new_course.js
@@ -1,7 +1,12 @@
 
+// Check if a user is logged-in, if not redirect the user to login page
+// Otherwise, display an alert message that a user is currently logged-in
 if (userId !== null && typeof userId === "string") {
   // User is logged-in
-    
+  // Check if user is not an admin
+  if (isAdmin === "false") {
+    history.back();
+  }
 } else {
   // User is not logged-in
   window.location.replace("./login.html");
