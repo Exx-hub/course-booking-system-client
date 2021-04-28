73a932623413e7957ce6ed294f96cd67533987e0
Glitch (hello-webpage) <none>
2021-04-28 12:26:02 +0000

    🎰🏋 Checkpoint ./courses.html:29751439/133 ./js/courses.js:29751439/928

--- js/courses.js
+++ js/courses.js
@@ -3,3 +3,14 @@ const createCourse = document.querySelector("#admin-button");
 if (!isAdmin || isAdmin === "false") {
   createCourse.innerHTML = null;
 }
+
+
+
+// retrieve all courses from databsae
+fetch("https://alvinacosta-csp2-app-server.herokuapp.com/api/courses")
+  .then(res => res.json())
+  .then(data => {
+    // console.log(data.courses);
+  
+    // display courses fetched from database on the course page  
+  });
