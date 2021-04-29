21d08ebd459e53a894913a64561d5dc2c1e70b77
Glitch (hello-webpage) <none>
2021-04-29 11:48:26 +0000

    🚈🛬 Checkpoint ./js/view_course.js:29751439/206

--- js/view_course.js
+++ js/view_course.js
@@ -1,1 +1,8 @@
-cosnt queryStringObject = new URLSearchParams(window.location.search);
+contt queryStringObject = new URLSearchParams(window.location.search);
+
+const courseId = queryStringObject.get("id");
+
+ // send a request to retrieve specific course using id
+fetch(`https://alvinacosta-csp2-app-server.herokuapp.com/api/courses?id={courseId}`, )
+
+
