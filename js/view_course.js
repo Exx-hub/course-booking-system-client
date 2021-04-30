3c3afb8a8133c5fa97f084561c5ec28d9f47880d
Glitch (hello-webpage) <none>
2021-04-30 13:26:43 +0000

    🎊🎇 Checkpoint ./js/view_course.js:29751439/117

--- js/view_course.js
+++ js/view_course.js
@@ -40,14 +40,14 @@ if (userId === null || isAdmin === "true") {
     })
       .then(res => res.json())
       .then(data => {
-        // console.dir(data.message);
+        console.dir(data.message);
 
-        if (data.message) {
-          alert("New enrollment to a course was successful.");
-          window.location.replace("./courses.html");
-        } else {
-          alert("Error: New enrollment to a course encountered a problem.");
-        }
+        // if (data.message) {
+        //   alert("New enrollment to a course was successful.");
+        //   window.location.replace("./courses.html");
+        // } else {
+        //   alert("Error: New enrollment to a course encountered a problem.");
+        // }
       });
   });
 }
